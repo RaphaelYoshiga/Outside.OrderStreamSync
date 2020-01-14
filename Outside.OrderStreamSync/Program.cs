008dbd142c76902366d01c5b898ea840278db800
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-01-14 22:25:07 +0000

    Add project files.

---
+++ Outside.OrderStreamSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Outside.OrderStreamSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
