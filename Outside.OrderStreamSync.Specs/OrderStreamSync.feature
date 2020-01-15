db2686b0a8729f55d195ae45f83a5e26393b4020
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-01-15 22:51:45 +0000

    Basic implementation of the spec on OrderStreamSync

---
+++ Outside.OrderStreamSync.Specs/OrderStreamSync.feature
@@ -0,0 +1,13 @@
+﻿Feature: OrderStreamSync
+	In order to ensure the backoffice system is up to date
+	As a operations person
+	I want website orders to be persisted in the backoffice
+
+Scenario: Sync simple order
+	Given we have this sales channel id 1 for AndroidApp
+	When A new order message arrives into the message queue
+	| OrderId | CustomerEmail | OrderAmount | SalesChannel |
+	| 9999    | test@test.com | 157.53      | AndroidApp   |
+	Then this order should have been persisted in the database
+	| OrderId | CustomerEmail | OrderAmount | SalesChannelId |
+	| 9999    | test@test.com | 157.53      | 1              |
---
+++ Outside.OrderStreamSync.Specs/OrderStreamSyncSteps.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Shouldly;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Outside.OrderStreamSync.Specs
+{
+    [Binding]
+    public class OrderStreamSyncSteps
+    {
+        private readonly Mock<ISalesChannelIdQuery> _salesChannelQueryMock;
+        private readonly OrderStreamSyncer _orderStreamSyncer;
+        private readonly Mock<IOrderPersister> _orderPersisterMock;
+
+        public OrderStreamSyncSteps()
+        {
+            _salesChannelQueryMock = new Mock<ISalesChannelIdQuery>();
+            _orderPersisterMock = new Mock<IOrderPersister>();
+            _orderStreamSyncer = new OrderStreamSyncer(_salesChannelQueryMock.Object, _orderPersisterMock.Object);
+        }
+
+        [Given(@"we have this sales channel id (.*) for (.*)")]
+        public void GivenWeHaveThisSalesChannelIdForSalesChannel(int salesChannelId, string salesChannel)
+        {
+            _salesChannelQueryMock.Setup(p => p.GetIdBy(salesChannel))
+                .ReturnsAsync(salesChannelId);
+        }
+        
+        [When(@"A new order message arrives into the message queue")]
+        public async Task WhenANewOrderMessageArrivesIntoTheMessageQueue(Table table)
+        {
+            var orderMessage = table.CreateInstance<OrderMessage>();
+            await _orderStreamSyncer.Sync(orderMessage);
+        }
+        
+        [Then(@"this order should have been persisted in the database")]
+        public void ThenThisOrderShouldHaveBeenPersistedInTheDatabase(Table table)
+        {
+            var backOfficeOrderExample = table.CreateInstance<BackOfficeOrderExample>();
+            _orderPersisterMock.Verify(p => p.Persist(It.Is<BackOfficeOrder>(actualOrder => backOfficeOrderExample.Matches(actualOrder))));
+        }
+    }
+
+    public class OrderStreamSyncer
+    {
+        public OrderStreamSyncer(ISalesChannelIdQuery salesChannelIdQuery, IOrderPersister orderPersister)
+        {
+        }
+
+        public Task Sync(OrderMessage orderMessage)
+        {
+            return Task.CompletedTask;;
+        }
+    }
+
+    public class OrderMessage
+    {
+        public string OrderId { get; set; }
+        public string CustomerEmail { get; set; }
+        public string SalesChannel { get; set; }
+        public decimal OrderAmount { get; set; }
+    }
+
+    public interface ISalesChannelIdQuery
+    {
+        Task<int> GetIdBy(string salesChannelDescription);
+    }
+
+    public interface IOrderPersister
+    {
+        Task Persist(BackOfficeOrder backOfficeOrder);
+    }
+
+    public class BackOfficeOrder
+    {
+        public string OrderId { get; set; }
+        public string CustomerEmail { get; set; }
+        public int SalesChannelId { get; set; }
+        public decimal OrderAmount { get; set; }
+    }
+
+    public class BackOfficeOrderExample
+    {
+        public string OrderId { get; set; }
+        public string CustomerEmail { get; set; }
+        public int SalesChannelId { get; set; }
+        public decimal OrderAmount { get; set; }
+
+        public bool Matches(BackOfficeOrder actualOrder)
+        {
+            actualOrder.OrderId.ShouldBe(OrderId);
+            actualOrder.CustomerEmail.ShouldBe(CustomerEmail);
+            actualOrder.SalesChannelId.ShouldBe(SalesChannelId);
+            actualOrder.OrderAmount.ShouldBe(OrderAmount);
+
+            return true;
+        }
+    }
+}
